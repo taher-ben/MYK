45ba54390db1fc2af93eb4b28c1c63c043029013
Taher BN <seazer914@gmail.com>
2024-02-14 19:49:06 +0200

    ''

---
+++ dashbord/dashbord-setting.js
@@ -0,0 +1,32 @@
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".fa-bars");
+sidebarBtn.onclick = function() {
+  sidebar.classList.toggle("active");
+  if(sidebar.classList.contains("active")){
+  sidebarBtn.classList.replace("fa-bars" ,"fa-bars-staggered");
+}else
+  sidebarBtn.classList.replace("fa-bars-staggered", "fa-bars");
+}
+
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+
+// function scrollFunction() {
+//   var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+//   if (
+//       document.body.scrollTop > 20 ||
+//       document.documentElement.scrollTop > 20
+//   ) {
+//       scrollToTopBtn.style.display = "block";
+//   } else {
+//       scrollToTopBtn.style.display = "none";
+//   }
+// }
+
+// function scrollToTop() {
+//   document.body.scrollTop = 0; 
+//   document.documentElement.scrollTop = -20;
+// }
